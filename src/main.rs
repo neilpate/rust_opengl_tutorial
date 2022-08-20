d027d5886c888ccce00a7a19df5e88bb323b0123
Neil <neil@premiergsolutions.com>
2022-08-20 10:53:47 +0200

    feat: Display a closable Window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate sdl2;
+
 fn main() {
     println!("Hello, world!");
+
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let window = video_subsystem
+        .window("Hi", 900, 700)
+        .resizable()
+        .build()
+        .unwrap();
+
+    let mut event_pump = sdl.event_pump().unwrap();
+
+    'main: loop {
+            for event in event_pump.poll_iter(){
+               //Handle user input here
+                match event {
+                    sdl2::event::Event::Quit { timestamp } => {print!("Program ran for {} ms", timestamp); break 'main},
+                    _ => {},
+                }     
+
+            }
+
+            //Render window contents here
+        }
+
 }
