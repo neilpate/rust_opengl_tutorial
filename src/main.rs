f73f161adbb68464d40fce6c1a4a33d01b7a648d
Neil <neil@premiergsolutions.com>
2022-08-21 15:16:33 +0200

    feat: Triangle now renders correctly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use gl::{types::{GLuint, GLsizeiptr, GLvoid, GLint}, ARRAY_BUFFER, STATIC_DRAW, FLOAT, FALSE, TRIANGLES};
+
 extern crate sdl2;
 extern crate gl;
 
@@ -27,42 +29,96 @@ fn main() {
     let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
 
-    unsafe {
-        gl::Viewport(0, 0, 900, 700); 
-        gl::ClearColor(1.0, 0.0, 0.0, 1.0); 
-    }
-
-    let mut event_pump = sdl.event_pump().unwrap();
-
+    
+    
     use std::ffi::CString;
-
+    
     let vert_shader = render_gl::Shader::from_vert_source(
         &CString::new(include_str!("triangle.vert")).unwrap()
     ).unwrap();
-
-    let frag_shader = render_gl::Shader::from_frag_source(
-        &CString::new(include_str!("triangle.frag")).unwrap()
+    
+    let frag_shader = render_gl::Shader::from_frag_source(&CString::new(include_str!("triangle.frag")).unwrap()
     ).unwrap();
+    
+    let shader_program = render_gl::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
 
-    let shader_program = render_gl::Program::from_shaders(
-        &[vert_shader, frag_shader]
-    ).unwrap();
+    // set up vertex buffer object
 
-    shader_program.set_used();
+    let vertices: Vec<f32> = vec![
+        -0.5, -0.5, 0.0,
+        0.5, -0.5, 0.0,
+        0.0, 0.5, 0.0
+        ];
+        
+    let mut vbo: GLuint = 0;
+    
+    unsafe { 
+        gl::GenBuffers(1, &mut vbo);  
+        gl::BindBuffer(ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            ARRAY_BUFFER, 
+            (vertices.len() * std::mem::size_of::<f32>()) as GLsizeiptr, 
+            vertices.as_ptr() as *const GLvoid, 
+            STATIC_DRAW
+        );
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+    }
 
 
-    'main: loop {
+    // set up vertex array object
+   
+    let mut vao: GLuint = 0;
+
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao); 
+        gl::BindVertexArray(vao);
+        gl::BindBuffer(ARRAY_BUFFER, vbo);
+        gl::EnableVertexAttribArray(0);
+        gl::VertexAttribPointer(
+            0, 
+            3, 
+            FLOAT, 
+            FALSE, 
+        (3 * std::mem::size_of::<f32>()) as GLint, 
+        std::ptr::null()
+    );
+    
+        gl::BindBuffer(ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+            }
+
+
+        unsafe {
+            gl::Viewport(0, 0, 900, 700); 
+            gl::ClearColor(1.0, 0.0, 0.0, 1.0); 
+        }
+        
+        
+        let mut event_pump = sdl.event_pump().unwrap();
+        
+        'main: loop {
             for event in event_pump.poll_iter(){
-               //Handle user input here
+                //Handle user input here
                 match event {
                     sdl2::event::Event::Quit { timestamp } => {print!("Program ran for {} ms", timestamp); break 'main},
                     _ => {},
                 }     
-
+                
             }
             
+            shader_program.set_used();
+            unsafe {
+                gl::BindVertexArray(vao);
+                gl::DrawArrays(
+                    TRIANGLES,
+                    0,
+                    3
+                );
+            }
+
             //Render window contents here
-            unsafe{ gl::Clear(gl::COLOR_BUFFER_BIT); }
+           // unsafe{ gl::Clear(gl::COLOR_BUFFER_BIT); }
 
             window.gl_swap_window();
 
